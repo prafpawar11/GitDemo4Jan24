4656fa41f8b57d5f81762fd2524629ed9cb921d8
prafpawar11 <prafpawar11@gmail.com>
2024-01-16 08:20:50 +0530

    Home Page TC

---
+++ src/main/java/PageLayer/HomePage.java
@@ -0,0 +1,12 @@
+package PageLayer;
+
+public class HomePage {
+
+	public void homePageTitle() {
+		System.out.println("Home Page Title");
+	}
+
+	public void homePageUrl() {
+		System.out.println("Home Page Url");
+	}
+}
